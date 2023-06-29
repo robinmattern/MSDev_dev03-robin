dbab054279acb667ff0647e6f4db81fd7071cd59
Robin Mattern <robin.mattern@gmail.com>
2023-06-29 01:46:51 +0000

    Changed title in index.html

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 async function run() {
-    console.log("running...");
+
+        console.log("running...");
+
     const config = {
         auth: {
             clientId: '<client-id>',
             authority: 'https://login.microsoftonline.com/organizations/',
             redirectUri: 'http://localhost:8080'
-        }
-    };
+            }
+        };
     var client = new msal.PublicClientApplication(config);
     
     var loginRequest = {
         scopes: [ 'user.read' ]
-    };
+        };
+
     let loginResponse = await client.loginPopup(loginRequest);
-    console.log('Login Response', loginResponse);
+        console.log('Login Response', loginResponse);
 
     var tokenRequest = {
         scopes: [ 'user.read' ],
         account: loginResponse.account
-    };
+        };
+
     let tokenResponse = await client.acquireTokenSilent(tokenRequest);
-    console.log('Token Response', tokenResponse);
+        console.log('Token Response', tokenResponse);
 
     let payload = await fetch("https://graph.microsoft.com/v1.0/me", {
         headers: {
             'Authorization': 'Bearer ' + tokenResponse.accessToken
-        }
-    });
+            }
+        });
+
     let json = await payload.json();
-    console.log('Graph Response', json);
+        console.log('Graph Response', json);
 }
